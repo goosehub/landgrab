7784d56e0b2a255e730df2ca26c7c0ab62cf0e72
Goose <goosepostbox@gmail.com>
2016-01-01 18:48:30 -0500

    sql and world_generator update
    Includes likely medium term starting point for land table data.

--- documents/world_generator.php
+++ documents/world_generator.php
@@ -7,11 +7,16 @@
 // 
 
 // World Key
-$world_key = 2;
+$world_key = 5;
+
+// World Slug
+$world_slug = 'foo';
+
+$world_tax_rate = '0.01';
 
 // Size of land box squares
-// Recommend between 1 and 4
-$box_size = 3;
+// Recommend 2, 3, 4, 6, or 12
+$box_size = 2;
 
 // Area covered defined with these limits
 // x and y limits must be evenly divisible by $box_size
@@ -21,17 +26,28 @@ $box_size = 3;
 $x_limit = 180;
 $y_limit = 84;
 
-// 
-// Logic
-// 
-
 $result = '';
 
-$insert_statement = "INSERT INTO `land` 
+// 
+// World Logic
+// 
+
+$world_insert_statement = "INSERT INTO `world` 
+(`id`, `slug`, `land_size`, `land_tax_rate`, `latest_rebate`, `created`, `modified`) 
+VALUES 
+(NULL, '" . $world_slug . "', '" . $box_size . "', '" . $world_tax_rate . "', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+
+$result .= $world_insert_statement;
+
+// 
+// Land Logic
+// 
+
+$land_insert_statement = "INSERT INTO `land` 
 (`id`, `coord_slug`, `lat`, `lng`, `world_key`, `claimed`, `account_key`, `land_name`, `price`, `content`, `primary_color`, `secondary_color`, `created`, `modified`) 
 VALUES";
 
-$result .= $insert_statement;
+$result .= $land_insert_statement;
 $result .= '<br>';
 
 // 
@@ -50,7 +66,7 @@ for ($x = -$x_limit; $x < $x_limit; $x = $x + $box_size) {
         {
             $result .= ';';
             $result .= '<br>';
-            $result .= $insert_statement;
+            $result .= $land_insert_statement;
             $result .= '<br>';
         } else {
             $result .= ',';
@@ -61,6 +77,8 @@ for ($x = -$x_limit; $x < $x_limit; $x = $x + $box_size) {
 }
 
 // Replace last comma with semi colon
+$search = ',';
+$replace = ';';
+$result = strrev(implode(strrev($replace), explode($search, strrev($result), 2)));
 
-$result = rtrim($result, ",");
-$result .= ';';
+echo $result;
