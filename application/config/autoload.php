e191dcac9f3fc10e46fa346454f2680ab25c7582
goosehub <goosepostbox@gmail.com>
2017-01-05 20:57:56 -0500

    Fix parse_crontab for cron
    See comments for more information. Should fix huge map reset issues.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,20 +88,54 @@ function get_time_ago_string($time_stamp, $divisor, $time_unit) {
 
 // http://stackoverflow.com/a/5727346/3774582
 // Parse CRON frequency
+// Break it down like James Brown
 function parse_crontab($time, $crontab) {
-    $time=explode(' ', date('i G j n w', strtotime($time)));
-    $crontab=explode(' ', $crontab);
-    foreach ($crontab as $k=>&$v) {
-        $v=explode(',', $v);
+    // Get current minute, hour, day, month, weekday
+    $time = explode(' ', date('i G j n w', strtotime($time)));
+    // Split crontab by space
+    $crontab = explode(' ', $crontab);
+    // Foreach part of crontab
+    foreach ($crontab as $k => &$v) {
+        // Remove leading zeros to prevent octal comparison, but not if number is already 1 digit
+        $time[$k] = preg_replace('/^0+(?=\d)/', '', $time[$k]);
+        // 5,10,15 each treated as seperate parts
+        $v = explode(',', $v);
+        // Foreach part we now have
         foreach ($v as &$v1) {
-            $v1=preg_replace(array(
-                '/^\*$/', '/^\d+$/', '/^(\d+)\-(\d+)$/', '/^\*\/(\d+)$/'),
-                array('true', $time[$k].'===\0', '(\1<='.$time[$k].' and '.$time[$k].'<=\2)', $time[$k].'%\1===0'),
+            // Do preg_replace with regular expression to create evaluations from crontab
+            $v1 = preg_replace(
+                // Regex
+                array(
+                    // *
+                    '/^\*$/',
+                    // 5
+                    '/^\d+$/',
+                    // 5-10
+                    '/^(\d+)\-(\d+)$/',
+                    // */5
+                    '/^\*\/(\d+)$/'
+                ),
+                // Evaluations
+                // trim leading 0 to prevent octal comparison
+                array(
+                    // * is always true
+                    'true',
+                    // Check if it is currently that time, 
+                    $time[$k] . '===\0',
+                    // Find if more than or equal lowest and lower or equal than highest
+                    '(\1<=' . $time[$k] . ' and ' . $time[$k] . '<=\2)',
+                    // Use modulus to find if true
+                    $time[$k] . '%\1===0'
+                ),
+                // Subject we are working with
                 $v1
             );
         }
-        $v='('.implode(' or ', $v).')';
+        // Join 5,10,15 with `or` conditional
+        $v = '(' . implode(' or ', $v) . ')';
     }
-    $crontab=implode(' and ', $crontab);
-    return eval('return '.$crontab.';');
+    // Require each part is true with `and` conditional
+    $crontab = implode(' and ', $crontab);
+    // Evaluate total condition to find if true
+    return eval('return ' . $crontab . ';');
 }
